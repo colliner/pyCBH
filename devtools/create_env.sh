949e515b4f58220f45a27ba379bb20988d237845
Eric Collins <colliner@github.iu.edu>
2020-12-15 15:00:10 -0500

    updated env

--- devtools/create_env.sh
+++ devtools/create_env.sh
@@ -7,6 +7,7 @@
 echo 'Creating conda environment for pyCBH'
 conda env create -q -f devtools/environment.yml --force
 git clone https://github.com/colliner/xyz2mol.git
+export $PYTHONPATH=$PATHONPATH:$PWD
 echo '# To activate this environment, use'
 echo '#'
 echo '#     $ conda activate pyCBH'
