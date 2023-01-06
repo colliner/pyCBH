84f30797767663a91510e608640b3c585a0a3cae
Eric M Collins <46455904+colliner@users.noreply.github.com>
2023-01-06 14:39:06 -0500

    Update pycbh.py

--- pycbh.py
+++ pycbh.py
@@ -3,35 +3,15 @@ import pycbh
 from rdkit import Chem
 import numpy as np
 
-print_cbh=True
-##print_cbh=False
-if print_cbh:
-  from pycbh import cbh_print
-else:
-  cbh_print = lambda c : print(['FAIL','PASS','PASS (batch)'][len(c[0:2])])
+from pycbh import cbh_print
 
 if __name__=='__main__':
   if len(sys.argv[1::]) < 2:
     sys.exit('Usage python3 pycbh.py CBH_rung FILENAME(s)')
-  save_graph = False
-  coarse_grain = False
-  fns=list()
-  for arg in sys.argv[2:]:
-    if arg == '-g':
-      save_graph = True
-    elif arg == '-cg':
-      coarse_grain = True
-    else:
-      fns.append(arg)
   try:
     rung_ls=[int(sys.argv[1])]
   except:
-    if sys.argv[1]=='all':
-      rung_ls=list(range(5))
-    elif '+' not in sys.argv[1]:
       sys.exit('Usage: python3 pycbh.py CBH_rung FILENAME(s)\n  CBH rung must be an integer')
-    else:
-      rung_ls=sys.argv[1]
   '''
   General conversion from files:
     filename(s) (.smi | .xyz | .mol) --> files2cbh --> cbh-n
